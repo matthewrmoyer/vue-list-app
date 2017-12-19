5cbba0c1a21b10e033917229759e78d570beb33d
Unknown <matthewrmoyer@gmail.com>
2017-12-19 12:21:53 -0700

    setup navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <app-navbar></app-navbar>
+    <li><router-link to="/navbar">Navbar</router-link></li>     
+
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    appNavbar: Navbar
+  }
 }
 </script>
 
@@ -18,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="hello">
-  <nav>
-    <div class="nav-wrapper">
+  <!-- <nav> -->
+    <!-- <div class="nav-wrapper">
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">JavaScript</a></li>
       </ul>
-    </div>
-  </nav>
+    </div> -->
+  <!-- </nav> -->
     <ul>
       <li>asdf</li>
       <li>asdf</li>
