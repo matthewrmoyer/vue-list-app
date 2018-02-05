76520902ba9fd15d48fc4d49d8e7577ba98d59b6
Unknown <matthewrmoyer@gmail.com>
2018-02-05 15:24:14 -0500

    Merge branch 'setup-board-cards'
    setup add item

--- src/store/modules/singleBoard/actions.js
+++ src/store/modules/singleBoard/actions.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 
 export const getBoard = async ({ commit }, payload) => {
-  let board = await (await Vue.http.get(`https://vue-board-backend.herokuapp.com/boards/singleboard/${payload}`)).json()
-  commit('setBoard', board)
+  try {
+    let board = await (await Vue.http.get(`https://vue-board-backend.herokuapp.com/boards/singleboard/${payload}`)).json()
+    commit('setBoard', board)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// payload = {id, item}
+export const addItem = async ({ commit }, payload) => {
+  console.log('addItem action dispatched');
+  console.log(payload);
+  try {
+    let board = await (await Vue.http.post(`https://vue-board-backend.herokuapp.com/boards/singleboard/${payload.id}`, payload.item)).json()
+    console.log(board)
+    commit('setBoard', board)
+  } catch (error) {
+    console.log(error)
+  }
 }
