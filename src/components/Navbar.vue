438b3570344eefaf93f1101c82f622a94ff6b792
Unknown <matthewrmoyer@gmail.com>
2018-02-09 15:56:06 -0500

    setup app manifest and navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,10 @@
     <div>
         <nav>
             <div class="nav-wrapper money-blue darken-1">
-                <a href="#!" class="brand-logo">Logo</a>
+                <!-- <a href="#!" class="brand-logo">Boards</a> -->
+                <router-link to="/" class="brand-logo">
+                    <i class="large material-icons">dashboard</i>
+                </router-link>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     <li><router-link to="/">Home</router-link></li>
